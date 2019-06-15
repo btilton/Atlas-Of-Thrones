77447974aa35d015ea79e729f51f1e08b56692e4
Brian <btilton@Brians-MBP.lan>
2019-06-15 16:32:53 -0700

    wait for webapp on startup to let user know serving is ready

--- start-atlas.sh
+++ start-atlas.sh
@@ -7,3 +7,21 @@ fi
 
 echo "start docker containers"
 docker-compose up -d
+
+retries=30
+
+printf "Waiting for webapp to start."
+while true; do
+    curl -Is localhost:8080 > /dev/null
+    if [ $? -ne 0 ]; then
+        printf "."
+    elif [ $retries -lt 1 ]; then
+        printf "\nStartup timed out\n"
+        exit 1
+    else
+        printf "\nStartup complete\n"
+        exit 0
+    fi
+    let retries--
+    sleep 1
+done
